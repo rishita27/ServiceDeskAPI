1a10135f4f0dac527948cacc87a2f95e8b036c8f
Rishita Kotiyal <rs677988@dal.ca>
2026-01-04 18:58:56 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/rkotiyal/servicedesk/ServiceDeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.rkotiyal.servicedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkotiyal/servicedesk/ServiceDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkotiyal.servicedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
