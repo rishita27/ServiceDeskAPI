2cf89db0b21483349c4ff915284385c95abd1bdc
Rishita Kotiyal <rs677988@dal.ca>
2026-01-05 20:02:32 -0400

    Add endpoint to create a booking

---
+++ src/main/java/com/rkotiyal/servicedesk/controller/BookingController.java
@@ -0,0 +1,29 @@
+package com.rkotiyal.servicedesk.controller;
+
+import com.rkotiyal.servicedesk.model.Booking;
+import com.rkotiyal.servicedesk.repository.BookingRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    private final BookingRepository repository;
+
+    public BookingController(BookingRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings(){
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking){
+        return repository.save(booking);
+    }
+}
---
+++ src/main/java/com/rkotiyal/servicedesk/model/Booking.java
@@ -0,0 +1,63 @@
+package com.rkotiyal.servicedesk.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+
+    private LocalDateTime bookingTime;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id")
+    private Service service;
+
+    private boolean canceled = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public void setCanceled(boolean canceled) {
+        this.canceled = canceled;
+    }
+}
